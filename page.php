1f7504f5905fd602c6c89bfb720e165834005cb6
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-07-04 18:30:39 -0300

    Otimizando portfolio

---
+++ page.php
@@ -0,0 +1,29 @@
+<?php
+include 'Header.phtml';
+?>
+
+<div class="container">
+    <img class="profile-img" src="" alt="">
+</div>
+
+<div class="container">
+    <div class="square">
+        <img class="profile-img" src="css/fotogabriel.png" alt="">
+        <h3>OLÁ, EU SOU O GABRIEL</h3>
+    </div>
+</div>
+<div class="container">
+    <h1>Sobre Mim</h1>
+    <p>
+        <b>
+            <h2>Olá, meu nome é Gabriel Eduardo Monsani, tenho 18 anos e estou cursando o terceiro período de Engenharia de Software na Faculdade Uniguaçu - FAESI. </h2>
+        </b>
+    </p>
+    <form action="http://localhost/bootcamp/contactpage/" method="get" target="_blank">
+        <button class="pulse-button" type="submit">CONTATE-ME</button>
+    </form>
+</div>
+
+<?php
+include 'Footer.phtml';
+?>
